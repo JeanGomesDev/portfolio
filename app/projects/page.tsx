5f6b2d6250904e8186368e60651a6a68b9dba46c
JeanGomesDaFonseca <jeangomes.dev@outlook.com>
2025-04-14 18:03:27 -0300

    starting creating page projects

---
+++ app/projects/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageIntroduction from '../pages/projects/page-introduction'
+
+const Projects = () => {
+  return (
+    <div>
+      <PageIntroduction/>
+    </div>
+  )
+}
+
+export default Projects

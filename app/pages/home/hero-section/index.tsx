ad8027302dcc4bb2f44bb9a4a4b229520da0c42d
JeanGomesDev <jeangomes.dev@outlook.com>
2025-05-03 15:28:45 -0300

    starting consuming for api hygraph

--- app/pages/home/hero-section/index.tsx
+++ app/pages/home/hero-section/index.tsx
@@ -53,7 +53,7 @@ const HeroSection = ({ homeInfo }: HomeSectionProps) => {
         <Image 
         width={420} 
         height={404} 
-        src={homeInfo.profilePicture.url} 
+        src="/images/profile-pic.jpeg" 
         alt="Foto de perfil do Jean Gomes"
         className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover" />
       </div>

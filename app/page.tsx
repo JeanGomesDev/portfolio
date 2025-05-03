579d2223b545e13cca32170c7ccc58cf12462acf
JeanGomesDev <jeangomes.dev@outlook.com>
2025-05-03 15:24:38 -0300

    consumido api do hygraph

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,43 @@ import HeroSection from "./pages/home/hero-section";
 import HighlightedProjects from "./pages/home/highlighted-projects";
 import KnownTechs from "./pages/home/known-techs";
 import WorkExperience from "./pages/home/work-experience";
+import { HomePageData } from "./types/page-info";
+import { fetchGygraphQuery } from "./utils/fetch-gygraph-query";
+
+const getPageData = async (): Promise<HomePageData> => {
+  const query = `
+  query PageInfoQuery {
+    page(where: {slug: "home"}) {
+      introduction {
+        raw
+      }
+      technologies {
+        name
+      }
+      profilePicture {
+        url
+      }
+      socials {
+        url
+        iconSvg
+      }
+      knownTechs {
+        iconSvg
+        name
+        startDate
+      }
+     }
+  }
+`
+  return fetchGygraphQuery(query, 60 * 60 * 24); 
+
+}
 
 export default async function Home() {
+  const {page: pageData} = await getPageData();
   return (
     <>
-      <HeroSection />
+      <HeroSection homeInfo={pageData} />
       <KnownTechs />
       <HighlightedProjects />
       <WorkExperience />

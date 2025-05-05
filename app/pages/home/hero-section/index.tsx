759825305c105813c6c44b256e383e8d5e8d86af
JeanGomesDev <jeangomes.dev@outlook.com>
2025-05-05 13:26:54 -0300

    refactor

--- app/pages/home/hero-section/index.tsx
+++ app/pages/home/hero-section/index.tsx
@@ -51,8 +51,8 @@ const HeroSection = ({ homeInfo }: HomeSectionProps) => {
           </div>
         </div>
         <Image 
-        width={420} 
-        height={404} 
+        width={420}
+        height={404}
         src="/images/profile-pic.jpeg" 
         alt="Foto de perfil do Jean Gomes"
         className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover" />
